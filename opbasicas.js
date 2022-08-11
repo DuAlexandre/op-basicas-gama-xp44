9475bcf867212592b35ec2abf6a63dbc9dc3f01e
Eduardo Alexandre <luis.edu.alex@gmail.com>
2022-08-11 14:07:53 -0300

    Adiciona operação de subtração

--- opbasicas.js
+++ opbasicas.js
@@ -8,6 +8,16 @@ function adicao () {
     console.log(`${numero1} mais ${numero2} e igual a ${resultado}`);
 }
 
+function subtracao () {
+    let numero1 = readline.question('Selecione o primeiro numero: ');
+    let numero2 = readline.question('Selecione o segundo numero: ');
+    let resultado = parseInt(numero1) - parseInt(numero2);
+    console.log(`${numero1} menos ${numero2} e igual a ${resultado}`);
+}
+
 if (entradaInicial.toLocaleLowerCase() === 'adicao') {
     adicao();
+}
+if (entradaInicial.toLocaleLowerCase() === 'subtracao') {
+    subtracao();
 }
