2a15d666262fc44bcaffecc30e93e613b917b95b
Eduardo Alexandre <luis.edu.alex@gmail.com>
2022-08-11 14:04:52 -0300

    Adiciona operação de adição

---
+++ opbasicas.js
@@ -0,0 +1,13 @@
+const readline = require('readline-sync');
+const entradaInicial = readline.question('Qual operacao deseja realizar? ');
+
+function adicao () {
+    let numero1 = readline.question('Selecione o primeiro numero: ');
+    let numero2 = readline.question('Selecione o segundo numero: ');
+    let resultado = parseInt(numero1) + parseInt(numero2);
+    console.log(`${numero1} mais ${numero2} e igual a ${resultado}`);
+}
+
+if (entradaInicial.toLocaleLowerCase() === 'adicao') {
+    adicao();
+}
